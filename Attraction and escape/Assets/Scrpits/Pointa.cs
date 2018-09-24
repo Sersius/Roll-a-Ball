7ac8c207f4c5f003b22d4d2035f2b3de4816ca2a
Sersius <segoro97@msn.com>
2018-09-24 12:34:56 +0200

    Attraction and escape

---
+++ Attraction and escape/Assets/Scrpits/Pointa.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointa : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject point;
+
+// Update is called once per frame
+void FixedUpdate ()
+    {
+        Vector3 direction = transform.position - point.transform.position;
+
+        Rigidbody rb = transform.GetComponent<Rigidbody>();
+
+        rb.AddForce(-direction);
+    }
+}
---
+++ Attraction and escape/Assets/Scrpits/Pointe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointe : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject point;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 direction = transform.position - point.transform.position;
+
+        Rigidbody rb = transform.GetComponent<Rigidbody>();
+
+        rb.AddForce(direction);
+    }
+}
